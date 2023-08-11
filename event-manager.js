bb5ecf253a9256a6c6cb8707dfced0f435d96a60
Victor <victorvlad19@gmail.com>
2023-08-11 12:20:42 +0300

    Added ui button

---
+++ event-manager.js
@@ -0,0 +1,32 @@
+AFRAME.registerComponent('event-manager', {
+
+    init: function () {
+        this.bindMethods();
+
+        let playing = false;
+
+        this.C3_Song = document.querySelector('#C3_Song');
+
+        this.C3 = document.querySelector('#C3');
+    },
+
+    bindMethods: function () {
+        this.onClick = this.onClick.bind(this);
+    },
+
+    onClick: function (evt) {
+        let targetEl = evt.target;
+
+        if (targetEl === this.C3) {
+
+            if (!playing) {
+                this.C3_Song.play();
+            } else {
+                this.C3_Song.pause();
+                this.C3_Song.currentTime = 0;
+            }
+            playing = !playing
+        }
+    }
+
+});
